e7a824f865a0d06d43b1b035f06e224bf51ccd4a
SouravvMishra <souravvmishra@gmail.com>
2023-05-14 23:19:19 +0530

    Create Prompt Working Fine

---
+++ app/layout.jsx
@@ -0,0 +1,32 @@
+import '@styles/globals.css'
+
+import Nav from '@components/Nav'
+import Provider from '@components/Provider'
+
+export const metadata = {
+  title: 'Promtopia',
+  description: 'Discover & Share AI Prompts',
+
+}
+
+const RootLayout = ({ children }) => {
+  return (
+    <html>
+      <body>
+        <Provider >
+          <div className='main'>
+
+          </div>
+          <div className='gradient' />
+          <main className='app'>
+            <Nav />
+            {children}
+
+          </main>
+        </Provider>
+      </body>
+    </html>
+  )
+}
+
+export default RootLayout;
---
+++ components/PromptCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PromptCard = () => {
+  return (
+    <div>
+      <h2>Prompt Card</h2>
+    </div>
+  )
+}
+
+export default PromptCard
