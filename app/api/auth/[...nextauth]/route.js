818e2f3db78f0f4dd2dff3c15205b0a6fc61d53c
SouravvMishra <souravvmishra@gmail.com>
2023-05-17 20:21:35 +0530

    Added Github Auth

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import GithubProvider from "next-auth/providers/github";
 
 import { connectToDB } from "@utils/database";
 import User from "@models/user";
@@ -10,7 +11,11 @@ const handler = NextAuth({
         GoogleProvider({
             clientId: process.env.GOOGLE_ID,
             clientSecret: process.env.GOOGLE_SECRET,
-        })
+        }), 
+        GithubProvider({
+            clientId: process.env.GITHUB_CLIENT_ID,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET
+          }),
     ],
     callbacks: {
         async session({ session }) {
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -5,9 +5,15 @@ import Image from "next/image"
 import { useState, useEffect } from "react"
 import { signIn, signOut, useSession, getProviders } from 'next-auth/react'
 
+
+
+
+
+
+
 const Nav = () => {
   // const [isUserLoggedIn, setIsUserLoggedIn] = useState(true)
-  const {data: session} = useSession();
+  const { data: session } = useSession();
   const [providers, setProviders] = useState(null)
   const [toggleDropdown, setToggleDropdown] = useState(false)
 
@@ -33,53 +39,47 @@ const Nav = () => {
           Promptopia
         </p>
       </Link>
-       
-       
+
+
 
       {/* Desktop navigation */}
 
       <div className="sm:flex hidden">
         {
-          session?.user ? (
-            <div className="flex gap-3 md:gap-5">
-              <Link href={'/create-prompt'}
-                className="black_btn"
-              >
-                Create Post
-              </Link>
-              <button type="button" onClick={signOut} className="outline_btn">
-                Sign Out
-              </button>
+          session?.user ?
+            (
 
-              
+              <div className="flex gap-3 md:gap-5">
+                <Link href={'/create-prompt'}
+                  className="black_btn"
+                >
+                  Create Post
+                </Link>
+                <button type="button" onClick={signOut} className="outline_btn">
+                  Sign Out
+                </button>
 
-              <Link href={'/profile'}>
-                <Image
-                  src={session?.user.image}
-                  width={37}
-                  height={37}
-                  className="rounded-full"
-                  alt="profile"
-                />
-              </Link>
 
-            </div>
-          ) : (
-            <>{
-              providers && Object.values(providers).map((provider) => {
-                return (
-                  <button
-                    key={provider.name}
-                    type="button"
-                    onClick={() => signIn(provider.id)}
-                    className="black_btn"
-                  >
-                    Sign In
-                  </button >
-                )
-              })
-            }</>
-          )
+
+                <Link href={'/profile'}>
+                  <Image
+                    src={session?.user.image}
+                    width={37}
+                    height={37}
+                    className="rounded-full"
+                    alt="profile"
+                  />
+                </Link>
+
+              </div>
+            ) : (
+              <>{(
+                <Link className="black_btn" href={'http://localhost:3000/api/auth/signin'} >
+                  Sign In
+                </Link>
+              )
+              }</>
+            )
         }
       </div>
 
@@ -126,23 +126,27 @@ const Nav = () => {
             </div>
           ) : (
             <>{
-              providers && Object.values(providers).map((provider) => {
-                return (
-                  <button
-                    key={provider.name}
-                    type="button"
-                    onClick={() => signIn(provider.id)}
-                    className="black_btn"
-                  >
-                    Sign In
-                  </button >
-                )
-              })
+              <Link className="black_btn" href={'http://localhost:3000/api/auth/signin'} >
+                  Sign in
+                </Link>
+              // providers && Object.values(providers).map((provider) => {
+              //   return (
+              //     <button
+              //       key={provider.name}
+              //       type="button"
+              //       onClick={() => signIn(provider.id)}
+              //       className="black_btn"
+              //     >
+              //       Sign In
+              //     </button >
+              //   )
+              // })
             }</>
 
           )
         }
       </div>
+
 
 
     </nav>
